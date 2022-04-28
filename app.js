483f8e53840e2a88ea297d9a6bb038474b5c535c
angelagonzalezp <angelaglezp@gmail.com>
2022-04-28 15:02:06 +0200

    remove comments

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
-// === imports == //
 import express from "express";
 import bodyParser from "body-parser";
 import indexRouter from "./scr/routes/index.js";
 import { toDoItems } from "./scr/models/todoitems.js";
 
-// === initialisation == //
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// === endpoints == //
-// index endpoint
 app.use("/", indexRouter);
 
-// get all tasks
 app.get("/task", (req, res) => {
   return res.json({ data: toDoItems, status: "success" });
 });
 
-// create a task
 app.post("/task", (req, res) => {
   toDoItems.push({
     index: getItemIndex(toDoItems),
@@ -28,17 +22,14 @@ app.post("/task", (req, res) => {
   return res.json({ data: toDoItems, status: "success" });
 });
 
-// delete a task
 app.delete("/task/:id", (req, res) => {
   let toDoItems = toDoItems.filter((d) => d.index != +req.params.id);
   return res.json({ data: toDoItems, status: "success" });
 });
 
-// update a task
 app.patch("/task/:id", (req, res) => {
   toDoItems.filter((d) => d.index == +req.params.id)[0].done = req.body.done;
   return res.json({ data: toDoItems, status: "success" });
 });
 
-// === run app == //
 app.listen(8000, () => console.log(`Example app running!`));
